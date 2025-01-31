c948264281433d14fe384809d4dae3a7875f5aa6
Nathangr06 <nathan.griguer@etu.univ-amu.fr>
2025-01-31 17:26:02 +0100

    modif

--- Exercice1/hello.java
+++ Exercice1/hello.java
@@ -2,7 +2,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		System.out.println("hello world !");
-		System.out.println("hola mundo !");
+		System.out.println("hola mun !");
 	}
 
 }
